32454362cc88e064800b209bcd032e510b68ec96
ManoelSan <naoanjos@gmail.com>
2023-01-11 15:18:23 -0300

    Segundo exercício consoantes

---
+++ Exercicios/src/arrays/Exerc02_Consoantes.java
@@ -0,0 +1,26 @@
+package arrays;
+import java.util.Scanner;
+//programaque ler um vetor de 6 caracteres
+//e mostre quantas consoates foram lidas
+
+public class Exerc02_Consoantes {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        String[] consoantes = new String[6];
+        int qtdConsoantes = 0;
+
+        int count = 0;
+        do{
+            System.out.println("Letras: ");
+            String letra = scan.next();
+        }
+        while(count < consoantes.length);
+        {
+
+        }
+
+     }
+}
+    
+
